2da70eb6d6950b84e514fe31828b0762b73ec39e
Xavie <Xavier.robles.r@gmail.com>
2020-05-14 10:19:13 +0200

    Formulario de registro

---
+++ FormularioDeRegistro.js
@@ -0,0 +1,8 @@
+function enfocado(id_objeto) {
+    document.getElementById(id_objeto).style.border="2px solid red";
+    
+}
+function desenfocado(id_objeto) {
+    document.getElementById(id_objeto).style.border="inherit";
+    
+}
